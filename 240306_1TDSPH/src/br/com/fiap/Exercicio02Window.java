7af3b2eaa5b997b461e515cf2f6e99b46408ab82
P3terHenry <pedro.antonieti@gmail.com>
2024-03-10 23:22:32 -0300

    Criado código para exibição em janela do cálculo de idade

--- 240306_1TDSPH/src/br/com/fiap/Exercicio02Window.java
+++ 240306_1TDSPH/src/br/com/fiap/Exercicio02Window.java
@@ -1,10 +1,24 @@
 package br.com.fiap;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio02Window {
 
 	public static void main(String[] args) {
 		
-
+		int ano_atual = 0, ano_nascimento = 0;
+		String auxiliar;
+		try {
+			auxiliar = JOptionPane.showInputDialog("Digite o ano atual:");
+			ano_atual = Integer.parseInt(auxiliar);
+			auxiliar = JOptionPane.showInputDialog("Digite a sua data de nascimento:");
+			ano_nascimento = Integer.parseInt(auxiliar);
+			JOptionPane.showMessageDialog(null, "Ano atual: " + ano_atual +
+					"\nAno de Nascimento: " + ano_nascimento + "\nSua idade é: " +
+					(ano_atual - ano_nascimento));
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Formato do número incorreto.");
+		}
 	}
 
 }
